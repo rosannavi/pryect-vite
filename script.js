798d8c51eeebe6e1620941750949b83edb8f2d3d
Alex <layoavicente3027@gmail.com>
2026-09-05 09:03:58 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+document.querySelector(".modal__button-close").addEventListener("click", () => {
+    document.querySelector(".modal").
+    style.display = "none";
+})
+document.querySelector(".abrir-modal").addEventListener("click", () => {
+    document.querySelector(".modal").
+    style.display = "block";
+})
